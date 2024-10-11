9ea2add12ec9538de8ff89fdaea6d33155bf8d59
Princemashumu <113059693+Princemashumu@users.noreply.github.com>
2024-10-11 10:50:57 +0200

    changed the module from express to HTTP

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-const express = require('express');
-const app = express();
+const http = require('http');
 
 // Use PORT from environment variables or default to 3000
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const requestListener = (req, res) => {
+  // Set the response HTTP header with HTTP status and Content type
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-// Start the server and listen on the specified port
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  // Send the response body "Hello World!"
+  res.end('Hello World!');
+};
+
+// Create an HTTP server and listen on the specified port
+const server = http.createServer(requestListener);
+
+server.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
 });
